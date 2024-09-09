12a31528740e23649a6023ef112c0c318150b8f4
Shivam Sharma <Shivam_Sharma1@ext.icicipruamc.com>
2024-09-09 19:46:02 +0530

    done basic API call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  //ON LOad
+
+  // useEffect(() => {
+  //   axios.get(`https://jsonplaceholder.typicode.com/users`).then((result) => {
+  //     console.log("result ==> ", result?.data);
+  //     if (result?.data?.length) {
+  //       setUsers(result?.data);
+  //     }
+  //   });
+  // }, []);
+
+  //On Btn Click
+  const loadUser = (e) => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then((result) => {
+      console.log("result ==> ", result?.data);
+      if (result?.data?.length) {
+        setUsers(result?.data);
+      }
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-         Home Page
-        </p>
-        <button>{"Load User"}</button>
+        <p>Home Page</p>
+        <button onClick={loadUser}>{"Load User"}</button>
+        {/* {users?.map((userObj) => {
+          return <div>{userObj?.name}</div>;
+        })} */}
+        <ol>
+          {users?.map((userObj) => {
+            return <li>{userObj?.name}</li>;
+          })}
+        </ol>
       </header>
     </div>
   );
-}
+};
 
 export default App;
